32bfd7bd08840de9b9eeea1e40f7e34089038bf1
Kirsten <kirsten.vermeulen@student.howest.be>
2020-08-31 18:16:35 +0200

    Frontend V3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
-let customers;
+let page, customers;
 
-const getDOMs = function() {
-    customers = document.querySelectorAll('.customer')
-    console.log(customers[1]['childNodes'][1])
+const getDOMsByPage = function(page) {
+
+    if(page == 'js-dashboard'){
+    }
+    else if(page == 'js-customers'){
+        customers = document.querySelectorAll('.customer')
+    }
+    else if(page == 'js-maintenance'){
+    }
+
+    
 }
 
-const addListeners = function() {
-    for(let customer of customers) {
-        customer['childNodes'][1].addEventListener('click', function() {
-            customer.classList.toggle('expand')
-        })
+const addListeners = function(page) {
+
+    if(page == 'js-dashboard'){
+    }
+    else if(page == 'js-customers'){
+        for(let customer of customers) {
+            customer['childNodes'][1].addEventListener('click', function() {
+                customer.classList.toggle('expand')
+            })
+        }
+    }
+    else if(page == 'js-maintenance'){
+    }    
+}
+
+const getData = function() {
+    if(page == 'js-dashboard'){
+        
+    }
+    else if(page == 'js-customers'){
+        fetch('http://127.0.0.1:5000/installaties')
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => console.error(`Error: ${error}`));
+    }
+    else if(page == 'js-maintenance'){
     }
 }
 
@@ -19,8 +48,11 @@ const init = function() {
     .then(json => console.log(json))
     .catch(error => console.error(`Error: ${error}`)); */
 
-    getDOMs()
-    addListeners()
+    page = document.querySelector('body').classList.value
+
+    getData()
+    getDOMsByPage(page)
+    addListeners(page)
 
 };
   
