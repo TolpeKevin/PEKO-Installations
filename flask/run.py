b9137bebbfc11590a09c814b7bf480a5ef31122b
Kevin <kevin.tolpe@student.howest.be>
2020-08-31 15:33:18 +0200

    fixed route klanten en installaties

--- flask/run.py
+++ flask/run.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
+import uuid
 
 # App
 
@@ -28,28 +29,35 @@ def init():
 def nieuwe_klant():
     with sqlite3.connect(db) as con:
             c = con.cursor()
-            nieuwe_klant = c.execute("INSERT INTO klanten VALUES (%s, %s, %s, %s)", [request.form['klant'], request.form['naam'], request.form['telefoon'], request.form['mail']])
+            nieuwe_k = c.execute("INSERT INTO klanten VALUES (%s, %s, %s, %s)", uuid.uuid4(), request.form['naam'], request.form['telefoon'], request.form['mail'])
+    return jsonify(nieuwe_k)
 
+@app.route("/nieuw/installatie")
+def nieuwe_intallatie():
+    with sqlite3.connect(db) as con:
+        c = con.cursor()
+        nieuwe_i = c.execute("INSERT INTO onderhoudsets VALUES (%s, %s, %s, %s, %s, %s, %s,%s)",uuid.uuid4(), [request.form['klant'], request.form['adres'], request.form['type'], request.form['datum_installatie'],request.form['datum_installatie'], request.form['onderhoud_peko'], request.form['onderhoud_atag'], request.form['p_nummer'], request.form['dagen_tot_onderhoud']])
+    return jsonify(nieuwe_i)
 
-            nieuwe_klant = c.execute("INSERT INTO klanten VALUES (%s, %s, %s, %s, %s, %s, %s)", [request.form['klant'], request.form['adres'], request.form['type'], request.form['datum_installatie'],request.form['onderhoud_peko'], request.form['onderhoud_atag'], request.form['p_nummer']])
-    return jsonify(nieuwe_klant)
 
 @app.route("/klanten")
 def klanten():
     with sqlite3.connect(db) as con:
         c = con.cursor()
         klanten_cursor = c.execute("select * FROM klanten")
-        klanten = [{"id":k[0],"naam":k[1],"adres":k[2],"type":k[3],"datum_installatie":k[4],"onderhoud_peko":k[5],"onderhoud_atag":k[6],"p_nummer":k[1]} for k in klanten_cursor]
+        klanten = [{"id":k[0],"naam":k[1],"telefoon":k[2],"mail":k[3]} for k in klanten_cursor]
     return jsonify(klanten)
 
-@app.route("/verwijder")
-def verwijder():
-    pass
+@app.route("/installaties")
+def instalaties():
+    with sqlite3.connect(db) as con:
+        c = con.cursor()
+        installaties_cursor = c.execute("select * FROM installaties")
+        instalaties = [{"id":k[0],"klant":k[1],"adres":k[2],"type":k[3],"datum_intallatie":k[4],"laatste_onderhoud":k[5],"onderhoud_peko":k[6],"onderhoude_atag":k[7],"p_nummer":k[8],"dagen_tot_onderhoud":k[9],"mail_verstuurd":k[10]} for k in installaties_cursor]
+    return jsonify(instalaties)
 
-@app.route("/update")
-def update():
-    klant = c.execute("UPDATE klanten set klant = %s, adres = %s, type = %s, datum_installatie= %s, onderhoud_peko = %s, onderhoud_atag = %s, p_nummer = %s)", [request.form['klant'], request.form['adres'], request.form['type'], request.form['datum_installatie'],request.form['onderhoud_peko'], request.form['onderhoud_atag'], request.form['p_nummer']])
-    return jsonify(klant)
+# uuid
+
 
 # Setup
 
